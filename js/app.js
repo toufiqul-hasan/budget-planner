046810fc31037c1877fb3bb527a720616326cfb7
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-02-17 08:45:37 +0600

    savings and remaining balance calculation added

--- js/app.js
+++ js/app.js
@@ -7,28 +7,44 @@ function getInputValue(inputId) {
   return amountValue;
 }
 
+// Handle Calculate Button
 document.getElementById("calculate-button").addEventListener("click", function () {
   // Getting Input Values
   const incomeAmount = getInputValue("income-input");
   const foodCostAmount = getInputValue("food-cost");
   const rentCostAmount = getInputValue("rent-cost");
   const clothesCostAmount = getInputValue("clothes-cost");
-  // Total Cost Calculation
+  // Cost Calculation
   const totalCost = foodCostAmount + rentCostAmount + clothesCostAmount;
-  // Total Balance Calculation
+  // Balance Calculation
   const balance = incomeAmount - totalCost;
-  // Update Total Balance
-  const balanceTotal = document.getElementById("balance");
-  const balanceText = balanceTotal.innerText;
-  const totalBalance = parseFloat(balanceText) + balance;
-  balanceTotal.innerText = totalBalance;
   // Update Total Expenses
   const expensesTotal = document.getElementById("total-expenses");
   const expensesText = expensesTotal.innerText;
   const totalExpenses = parseFloat(expensesText) + totalCost;
   expensesTotal.innerText = totalExpenses;
-});
-
-document.getElementById("save-button").addEventListener("click", function () {
-  const savingsAmount = getInputValue("savings-input");
+  // Update Balance
+  const balanceTotal = document.getElementById("balance");
+  const balanceText = balanceTotal.innerText;
+  const totalBalance = parseFloat(balanceText) + balance;
+  balanceTotal.innerText = totalBalance;
+  // Handle Save Button
+  document.getElementById("save-button").addEventListener("click", function () {
+    // Getting Input Value
+    const savingsInput = getInputValue("savings-input");
+    // Saving Calculation
+    const saving = (incomeAmount * savingsInput) / 100;
+    // Remaining Balance Calculation
+    const remainingBalance = balance - saving;
+    // Update Saving Amount
+    const savingAmount = document.getElementById("saving-amount");
+    const savingAmountText = savingAmount.innerText;
+    const totalSavingAmount = parseFloat(savingAmountText) + saving;
+    savingAmount.innerText = totalSavingAmount;
+    // Update Remaining Balance
+    const remainingBalanceAmount = document.getElementById("remaining-balance");
+    const remainingBalanceAmountText = remainingBalanceAmount.innerText;
+    const totalRemainingBalanceAmount = parseFloat(remainingBalanceAmountText) + remainingBalance;
+    remainingBalanceAmount.innerText = totalRemainingBalanceAmount;
+  });
 });
